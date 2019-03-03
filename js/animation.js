39b982e83e7e8e879e2efe8c3e64aac98c3a9e9d
Patrick Schuster <patrick.schustermail@gmail.com>
2019-03-03 18:04:08 +0100

    Set nr of stars according to view width.

--- js/animation.js
+++ js/animation.js
@@ -1,16 +1,18 @@
 let canvas;
 let stars = [];
-let numberOfStars = 300;
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
-    numberOfStars = 50;
+    setup();
 }
 
 function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
     canvas.position(0, 0);
     canvas.style('z-index', '-1');
+
+    stars = [];
+    numberOfStars = Math.floor(windowWidth / 3);
 
     for(let i = 0; i < numberOfStars; i++){
         stars[i] = new Star();
@@ -41,30 +43,13 @@ class Star {
         // stars with smaller z are smaller, stars with larger z are larger 
         const size = map(this.z, 0, width, 10, 0);
         fill(255);
-        //drawStar(sx, sy, 1, size, size);
         ellipse(sx, sy, size, size);
     }
 }
 
-// experimental
-function drawStar(x, y, radius1, radius2, npoints) {
-    let angle = TWO_PI / npoints;
-    let halfAngle = angle / 2.0;
-    beginShape();
-    for (let a = 0; a < TWO_PI; a += angle) {
-        let sx = x + cos(a) * radius2;
-        let sy = y + sin(a) * radius2;
-        vertex(sx, sy);
-        sx = x + cos(a + halfAngle) * radius1;
-        sy = y + sin(a + halfAngle) * radius1;
-        vertex(sx, sy);
-    }
-    endShape(CLOSE);
-}
-
 function draw() {
     background(0);
-    translate(width/2, height/2);
+    translate(windowWidth/2, windowHeight/2);
     for(let i = 0; i < stars.length; i++){
         stars[i].update();
         stars[i].show();
